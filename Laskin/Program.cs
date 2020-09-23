411b5771df1867dac44a1891c9c289c7bdf0fd25
minnaiik <50232682+minnaiik@users.noreply.github.com>
2020-09-23 14:02:33 +0300

    Add files via upload

---
+++ Laskin/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Laskin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anna ensimmäinen luku:");
+            String ekaluku = Console.ReadLine();
+            Console.WriteLine("Anna toinen luku:");
+            String tokaluku = Console.ReadLine();
+
+            int summa = int.Parse(ekaluku) + int.Parse(tokaluku);
+            Console.WriteLine("Summa on " + summa);
+        }
+    }
+}
